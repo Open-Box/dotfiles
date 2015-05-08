b661ec04e49790e8259090010e93cfe0bb73a67c
bado <a.badii@open-box.it>
2015-05-08 11:59:52 +0200

    added new useful (?) alias created and empty file for more aliases added "extract" function for managing unarchive of files

--- 00_bash/alias.bash
+++ 00_bash/alias.bash
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
+########################
+
 # Pipe my public key to my clipboard.
 #alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 ### Alias per forzare sovrascrittura pacchetti
-#alias apt-get="sudo apt-get -o Dpkg::Options::=\"--force-overwrite\""  
-
-
+#alias apt-get="sudo apt-get -o Dpkg::Options::=\"--force-overwrite\""
 
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -18,14 +19,152 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+
+### some more ls aliases
+########################
+alias ll='ls -alF'
+#alias ll='ls -AlF' ## con si omette la parent e la current directory
+alias la='ls -A'
+alias l='ls -CF' # Make ls display in columns and with a file type indicator (end directories with "/", etc) by default
+
+### moving on directories
+################################
+
+alias cd..="cd .."
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias ....="cd ../../../.."
+alias .....="cd ../../../../.."
+
+### System Aliases
+##################
+
+# List our disk usage in human-readable units including filesystem type, and print a total at the bottom:
+alias df="df -Tha --total"
+
+#preferred du output as well:
+#alias du="du -ach | sort -h"
+alias du="du -ach"
+
+# Make free output more human friendly:
+alias free="free -mt"
+
+#default output listing process table.
+alias ps="ps auxf"
+
+# Make process table searchable. Ie: `psg bash`
+alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+
+## pass options to free ##
+alias meminfo='free -m -l -t'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+## Get server cpu info ##
+alias cpuinfo='lscpu'
+
+## older system use /proc/cpuinfo ##
+##alias cpuinfo='less /proc/cpuinfo' ##
+
+## get GPU ram on desktop / laptop##
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+
+
+
+### Networking
+##############
+
+# Have you ever needed your public IP address from the command line when you're behind a router using NAT? Something like this could be useful:
+alias myip="curl http://ipecho.net/plain; echo"
+
+# Stop after sending count ECHO_REQUEST packets #
+alias ping='ping -c 10'
+# Do not wait interval 1 second, go fast #
+alias fastping='ping -c 100 -s.2'
+
+#quickly list all TCP/UDP port on the server:
+alias ports='netstat -tulanp'
+
+# get web server headers #
+alias header='curl -I'
+
+# find out if remote server supports gzip / mod_deflate or not #
+alias headerc='curl -I --compress'
+
+### Continue getting a partially-downloaded file. This is useful when you want
+### to finish up a download started by a previous instance of Wget, or
+### by another program
+alias wget='wget -c'
+
+# Wake-on-LAN (WOL) is an Ethernet networking standard that allows
+# a server to be turned on by a network message. You can quickly wakeup
+# nas devices and server using the following aliases:
+## replace mac with your actual server mac address #
+#alias wakeupnas01='/usr/bin/wakeonlan 00:11:32:11:15:FC'
+#alias wakeupnas02='/usr/bin/wakeonlan 00:11:32:11:15:FD'
+#alias wakeupnas03='/usr/bin/wakeonlan 00:11:32:11:15:FE'
+
+### Networking - Firewall
+########################
+# Control firewall (iptables) output
+## shortcut  for iptables and pass it via sudo#
+alias ipt='sudo /sbin/iptables'
+
+# display all rules #
+#alias iptlist='sudo /sbin/iptables -L -n -v --line-numbers'
+#alias iptlistin='sudo /sbin/iptables -L INPUT -n -v --line-numbers'
+#alias iptlistout='sudo /sbin/iptables -L OUTPUT -n -v --line-numbers'
+#alias iptlistfw='sudo /sbin/iptables -L FORWARD -n -v --line-numbers'
+#alias firewall=iptlist
+
+### WebServers Control
+######################
+# also pass it via sudo so whoever is admin can reload it without calling you #
+#alias nginxreload='sudo /usr/local/nginx/sbin/nginx -s reload'
+#alias nginxtest='sudo /usr/local/nginx/sbin/nginx -t'
+#alias lightyload='sudo /etc/init.d/lighttpd reload'
+#alias lightytest='sudo /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -t'
+#alias httpdreload='sudo /usr/sbin/apachectl -k graceful'
+#alias httpdtest='sudo /usr/sbin/apachectl -t && /usr/sbin/apachectl -t -D DUMP_VHOSTS'
+
+
+#### Miscellaneous
+##################
+
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+
+
+
+
+#sudo apt-get install imagemagick
+# For my own purposes, I like to optimize the images I upload for articles to be 690px or less, so I use the ImageMagick package (sudo apt-get install imagemagick if not already available) which contains a command called mogrify that does just this. I have this command in my ~/.bashrc file:
+alias webify="mogrify -resize 690\> *.png"
+#This will resize all of the PNG images in the current directory, only if they are wider than 690px.
+
+# install  colordiff package :)
+alias diff='colordiff'
+
+# most used command in histroy
+#alias usedcmds="history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10"
+
+
 
 # Alias definitions.
 # You may want to put all your additions into a separate file like
@@ -35,4 +174,3 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
-
--- 00_bash/functions.bash
+++ 00_bash/functions.bash
@@ -1,7 +1,7 @@
 # Source a file or .bash files in a directory
 function src_file() {
     local file
-    
+
     if [ 1 == $(is_regular_file $1) ]; then
         #source "$DOTFILES/source/$1.sh"
         source $1
@@ -14,7 +14,7 @@ function src_file() {
         for file in $1/*.bash; do
             source "$file"
         done
-        
+
         ### source dei completion
         for file in $1/*.completion; do
             source "$file"
@@ -51,30 +51,30 @@ function get_os() {
 
 ### test files
 function is_regular_file() {
-  if  ! [[ -L $1 || -d $1 ]] 
-  then echo 1; 
-  else echo 0; 
+  if  ! [[ -L $1 || -d $1 ]]
+  then echo 1;
+  else echo 0;
   fi
 }
 
 function is_regular_directory() {
-  if  ! [[ -L $1 || -f $1 ]] 
-  then echo 1; 
-  else echo 0; 
+  if  ! [[ -L $1 || -f $1 ]]
+  then echo 1;
+  else echo 0;
   fi
 }
 
 function is_symlink_file() {
-  if  [[ -L $1 && -f $1 ]] 
-  then echo 1; 
-  else echo 0; 
+  if  [[ -L $1 && -f $1 ]]
+  then echo 1;
+  else echo 0;
   fi
 }
 
 function is_symlink_directory() {
-  if  [[ -L $1 && -d $1 ]] 
-  then echo 1; 
-  else echo 0; 
+  if  [[ -L $1 && -d $1 ]]
+  then echo 1;
+  else echo 0;
   fi
 }
 
@@ -95,3 +95,36 @@ function user_question()    { printf "\r [ \033[0;33m?\033[0m ] $1 \n"; }
 randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;}
 
 
+### Extract combines a lot of utilities to allow you to decompress just about any compressed file format
+# from https://github.com/xvoland/Extract/blob/master/extract.sh
+function extract {
+ if [ -z "$1" ]; then
+    # display usage if no parameters given
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+ else
+    if [ -f $1 ] ; then
+        # NAME=${1%.*}
+        # mkdir $NAME && cd $NAME
+        case $1 in
+          *.tar.bz2)   tar xvjf ../$1    ;;
+          *.tar.gz)    tar xvzf ../$1    ;;
+          *.tar.xz)    tar xvJf ../$1    ;;
+          *.lzma)      unlzma ../$1      ;;
+          *.bz2)       bunzip2 ../$1     ;;
+          *.rar)       unrar x -ad ../$1 ;;
+          *.gz)        gunzip ../$1      ;;
+          *.tar)       tar xvf ../$1     ;;
+          *.tbz2)      tar xvjf ../$1    ;;
+          *.tgz)       tar xvzf ../$1    ;;
+          *.zip)       unzip ../$1       ;;
+          *.Z)         uncompress ../$1  ;;
+          *.7z)        7z x ../$1        ;;
+          *.xz)        unxz ../$1        ;;
+          *.exe)       cabextract ../$1  ;;
+          *)           echo "extract: '$1' - unknown archive method" ;;
+        esac
+    else
+        echo "$1 - file does not exist"
+    fi
+fi
+}
